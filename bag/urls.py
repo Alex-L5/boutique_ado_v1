78db138f300c0ffb50ede7fe7d1a225ecb0345b8
Alex-L5 <almenkiti@aol.com>
2026-06-14 13:39:53 +0100

    Add add to bag functionality

--- bag/urls.py
+++ bag/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.view_bag, name='view_bag'),
+    path('add/<item_id>/', views.add_to_bag, name='add_to_bag'),  # URL containing the item id will return our add_to_bag view and will be named add_to_bag
 ]
--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
@@ -6,3 +6,19 @@ def view_bag(request):
     """ A view that renders the bag contents page """
     
     return render(request, 'bag/bag.html')
+
+def add_to_bag(request, item_id):  # submitting the form to this view including the product id and the quantity
+    """ Add a quantity of the specified product to the shopping bag """
+
+    quantity = int(request.POST.get('quantity'))  # getting the quantity from the form and converting it to an integer since it'll come from the template as a string    
+    redirect_url = request.POST.get('redirect_url')
+    bag = request.session.get('bag', {})  # a HTTP session is used to allow information to be stored by the user. the variable bag (dictionary) accesses the requests session
+# once in the view we'll get the bag variable if it exists in the session or create it if it doesn't
+    if item_id in list(bag.keys()):  # if the item is already in the bag (a key in the bag dictionary matching this product id) then its quantity is incremented accordingly
+        bag[item_id] += quantity
+    else:
+        bag[item_id] = quantity  # a key of the items id within the bag dictionary (adding the item to the bag)  
+
+    request.session['bag'] = bag  # putting the bag variable into the session or updating it    
+    print(request.session['bag'])  # printing the shopping bag from the session in the add to bag view  
+    return redirect(redirect_url)  # redirecting the user back to the redirect URL
